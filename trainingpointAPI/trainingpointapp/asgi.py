45fae4f06f37a9e26f592c1baa3450d6a0db0bfb
Phạm Ngọc Trường Sơn <sonpnts@ou.edu.vn>
2024-05-24 14:40:19 +0700

    xong model

--- trainingpointAPI/trainingpointapp/asgi.py
+++ trainingpointAPI/trainingpointapp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for managetrainingpoint project.
+ASGI config for trainingpointappp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'managetrainingpoint.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainingpointappp.settings')
 
 application = get_asgi_application()
--- trainingpointAPI/trainingpointapp/wsgi.py
+++ trainingpointAPI/trainingpointapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for managetrainingpoint project.
+WSGI config for trainingpointappp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'managetrainingpoint.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainingpointappp.settings')
 
 application = get_wsgi_application()
